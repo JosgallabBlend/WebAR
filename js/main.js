2c132165054d9d674835840dc15e0be407611171
g12345orgo <g12345orgo@gmail.com>
2025-05-11 19:44:26 +0200

    Dos image tracking con audios distintos en un mismo <audio>

--- js/main.js
+++ js/main.js
@@ -57,6 +57,9 @@ document.addEventListener('DOMContentLoaded', function () {
             audioDiv.style.alignItems = 'center';
 
             const audio = document.getElementById('audioClick');
+            var srcRoute = "audio/"+audio.id.toString();
+            console.log(srcRoute);
+            audio.setAttribute("src", srcRoute );
             audio.currentTime = 0;
             audio.play();
 
